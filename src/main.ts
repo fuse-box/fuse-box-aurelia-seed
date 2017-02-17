55f41ed330b2b13d09c925649a01f4c51d9a1547
arjendeblok <arjen@deblok.org>
2017-02-17 14:09:34 +0100

    Removed font-awesome.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 ﻿import {Aurelia} from 'aurelia-framework';
-// we want font-awesome to load as soon as possible to show the fa-spinner
-import '../styles/styles.css';
-import 'font-awesome/css/font-awesome.css';
+
+import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
 
