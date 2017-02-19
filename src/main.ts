e9c960bc28ae2ea32a383ffe1eb59d6a837bf1fe
arjendeblok <arjen@deblok.org>
2017-02-19 07:42:07 +0100

    Added a timeout to let splash spinner show.
    Spinner is not shown in IE because it is too busy with loading aurelia
    before it loads the font.

--- src/main.ts
+++ src/main.ts
@@ -29,8 +29,12 @@ export async function configure(aurelia: Aurelia) {
   // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   // aurelia.use.plugin('aurelia-html-import-template-loader')
 
-  await aurelia.start();
-  await aurelia.setRoot('app');
+  // set a timeout to let the splash screen show something (mostly in IE 11)
+  // this may not be needed for your app
+  window.setTimeout(async () => {
+    await aurelia.start();
+    await aurelia.setRoot('app');  
+  }, 50);
 
   // if you would like your website to work offline (Service Worker), 
   // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
