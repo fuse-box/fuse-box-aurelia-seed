423b08d5439a679d2957e0bf2c0dadeb10825f7c
vegar ringdal <vegar.ringdal@gmail.com>
2017-05-10 17:33:46 +0200

    fix(sidemenu)

---
+++ src/components/nav-bar.ts
@@ -0,0 +1,16 @@
+import { Router } from 'aurelia-router';
+import { inject } from 'aurelia-framework';
+declare var $: any;
+
+@inject(Router)
+export class NavBar {
+    public router: Router;
+
+    constructor(router: Router) {
+        this.router = router;
+    }
+
+    public attached() {
+        $('.button-collapse').sideNav();
+    }
+}
